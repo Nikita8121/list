1e1957b4350a8b2bbb0d02af5ccdf06b3ff64129
Nikita8121 <natanalegarh@gmail.com>
2023-11-16 20:53:37 +0200

    upd

--- src/TreeList.tsx
+++ src/TreeList.tsx
@@ -1,19 +1,11 @@
-import { useCallback, useState } from 'react';
+import { useMemo, useRef, useState } from 'react';
 import {
   UncontrolledTreeEnvironment,
   Tree,
   StaticTreeDataProvider,
   TreeItem,
-  /*   KeyboardBindings,
-  InteractionMode, */
+  DraggingPosition,
 } from 'react-complex-tree';
-
-/* type Item = {
-  index: string;
-  isFolder?: boolean;
-  children: string[];
-  data: string;
-}; */
 
 type Node = {
   id: string;
@@ -24,9 +16,9 @@ type NodeList = Node[];
 
 type TreeItems = { [key: string]: TreeItem<string> };
 
-const generateItem = (index: number): TreeItem<string> => ({
+/* const generateItem = (index: number): TreeItem<string> => ({
   index: `child${index}`,
-  isFolder: /* index % 2 === 0 */ true,
+  isFolder:  true,
   children: [],
   data: `Child item ${index}`,
 });
@@ -38,24 +30,79 @@ const mockItems: TreeItems = {
     children: Array.from({ length: 50 }, (_, i) => `child${i + 1}`),
     data: 'Root item',
   },
+}; */
+
+const nodeListToTreeItems = (nodeList: NodeList): TreeItems => {
+  const treeItems: TreeItems = {
+    root: {
+      index: 'root',
+      isFolder: true,
+      children: [],
+      data: 'Root item',
+    },
+  };
+
+  const nodeToTreeItem = (node: Node): TreeItem => {
+    const treeItem: TreeItem = {
+      index: node.id,
+      data: node.text,
+      isFolder: !!node.nodes.length,
+      children: node.nodes.map((childNode) => childNode.id),
+    };
+
+    node.nodes.forEach((childNode) => {
+      treeItems[childNode.id] = nodeToTreeItem(childNode);
+    });
+
+    return treeItem;
+  };
+
+  nodeList.forEach((node) => {
+    treeItems.root.children?.push(node.id);
+    treeItems[node.id] = nodeToTreeItem(node);
+  });
+
+  return treeItems;
 };
 
-const initialItems = {
-  root: {
-    index: 'root',
-    isFolder: true,
-    children: [],
-    data: 'Root item',
-  },
+const treeListItemsToNodeList = (items: TreeItems): NodeList => {
+  const treeItemToNode = (item: TreeItem<string>): Node => {
+    const node: Node = {
+      id: item.index.toString(),
+      text: item.data,
+      nodes: item.children
+        ? item.children.map((child) => treeItemToNode(items[child]))
+        : [],
+    };
+    return node;
+  };
+
+  const nodeList: NodeList = [];
+  const rootItem = items.root;
+  const children = rootItem.children;
+  children?.forEach((child) => {
+    const childItem = items[child];
+    const childItemNode: Node = treeItemToNode(childItem);
+    nodeList.push(childItemNode);
+  });
+  return nodeList;
 };
 
 export const TreeList = () => {
-  for (let i = 1; i <= 50; i++) {
+  /* for (let i = 1; i <= 50; i++) {
     mockItems[`child${i}`] = generateItem(i);
-  }
+  } */
 
-  /* const storedTree = localStorage.getItem('tree');
-const mockItems: TreeItems = storedTree ? nodeListToTree(JSON.parse(storedTree)) : []; */
+  const storedTree = useRef<string | null>(localStorage.getItem('tree'));
+  const mockItems: TreeItems = useMemo(
+    () =>
+      nodeListToTreeItems(JSON.parse(storedTree.current ?? '[]') as NodeList),
+    []
+  );
+
+  const treeItemsIdsOpened = Object.values(mockItems)
+    .filter((item) => item.children?.length)
+    .map((item) => item.index);
 
   const [isChangesApplied, setIsChangesApplied] = useState<boolean>();
 
@@ -63,62 +110,16 @@ const mockItems: TreeItems = storedTree ? nodeListToTree(JSON.parse(storedTree))
     setIsChangesApplied(true);
   };
 
-  const treeListItemsToNodeList = useCallback((items: TreeItems): NodeList => {
-    const treeItemToNode = (item: TreeItem<string>): Node => {
-      const node: Node = {
-        id: item.index.toString(),
-        text: item.data,
-        nodes: item.children?.length
-          ? item.children.map((child) => treeItemToNode(items[child]))
-          : [],
-      };
-      return node;
-    };
-
-    console.log('items', items);
-
-    const nodeList: NodeList = [];
-    const rootItem = items.root;
-    const children = rootItem.children;
-    children?.forEach((child) => {
-      const childItem = items[child];
-      const childItemNode: Node = treeItemToNode(childItem);
-      nodeList.push(childItemNode);
-    });
-    return nodeList;
-  }, []);
-
-  /*  const nodeListToTree = useCallback((nodeList: NodeList): Node => {
-    const tree: Node = {
-      id: 'root',
-      text: 'Root item',
-      nodes: [],
-    };
-
-    const addNodeToTree = (parentNode: Node, nodeToAdd: Node) => {
-      parentNode.nodes.push(nodeToAdd);
-    };
-
-    const addNodesToTree = (parentNode: Node, nodesToAdd: NodeList) => {
-      nodesToAdd.forEach((node) => {
-        const newNode = {
-          id: node.id,
-          text: node.text,
-          nodes: [],
-        };
-        addNodeToTree(parentNode, newNode);
-        addNodesToTree(newNode, node.nodes);
-      });
-    };
-
-    addNodesToTree(tree, nodeList);
-
-    return tree;
-  }, []); */
+  const handleDrop = (items: TreeItem<string>[], target: DraggingPosition) => {
+    console.log('dropped', items);
+    if (target.targetType === 'item')
+      mockItems[target.targetItem].isFolder = true;
+    handleTreeChange();
+  };
 
   const handleSaveChanges = () => {
     const node = treeListItemsToNodeList(mockItems);
-    console.log(node);
+    console.log('savedNode', node);
     localStorage.setItem('tree', JSON.stringify(node));
     setIsChangesApplied(false);
   };
@@ -128,24 +129,34 @@ const mockItems: TreeItems = storedTree ? nodeListToTree(JSON.parse(storedTree))
       {isChangesApplied && (
         <button onClick={handleSaveChanges}>Save Changes</button>
       )}
-      <UncontrolledTreeEnvironment
-        dataProvider={
-          new StaticTreeDataProvider(mockItems, (item, data) => ({
-            ...item,
-            data,
-          }))
-        }
-        getItemTitle={(item) => item.data}
-        viewState={{}}
-        canDragAndDrop={true}
-        canReorderItems={true}
-        canRename={true}
-        canDropOnFolder={true}
-        onRenameItem={handleTreeChange}
-        onDrop={handleTreeChange}
-      >
-        <Tree treeId="tree-2" rootItem="root" treeLabel="Tree Example" />
-      </UncontrolledTreeEnvironment>
+      <div style={{ maxHeight: '600px', overflowY: 'auto' }}>
+        <UncontrolledTreeEnvironment
+          dataProvider={
+            new StaticTreeDataProvider(mockItems, (item, data) => {
+              return {
+                ...item,
+                data,
+              };
+            })
+          }
+          viewState={{
+            ['tree-1']: {
+              expandedItems: treeItemsIdsOpened,
+            },
+          }}
+          getItemTitle={(item) => item.data}
+          canDragAndDrop={true}
+          canReorderItems={true}
+          canRename={true}
+          canDropOnFolder={true}
+          canDropOnNonFolder={true}
+          onSelectItems={(/* items, treeId */) => console.log('selected')}
+          onRenameItem={handleTreeChange}
+          onDrop={handleDrop}
+        >
+          <Tree treeId="tree-1" rootItem="root" treeLabel="Tree Example" />
+        </UncontrolledTreeEnvironment>
+      </div>
     </>
   );
 };
